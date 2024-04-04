f06136ad4cedaf3ad047904384311ad6e0e49ef0
Goryntsev Roman <roma.goryntsev2015@yandex.ru>
2024-04-04 14:01:01 +0300

    made homework1

--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -8,16 +8,19 @@ TEST_GRAPH_FILES = [
 
 
 def has_cycles(g: nx.Graph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
+    cycles = nx.cycle_basis(g)
+    #если метод возвращает непустой список, то цикл существует
+    if len(cycles) > 0:
+        print("Cycles found")
+        return True
+    else:
+        return False
     pass
 
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
         # Load the graph
-        G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.Graph)
+        G = nx.read_edgelist(f"D://algorithm//spbu-fundamentals-of-algorithms//practicum_2//homework//basic//{filename}", create_using=nx.Graph)
         # Output whether it has cycles
         print(f"Graph {filename} has cycles: {has_cycles(G)}")
